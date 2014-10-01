64c658cd34f314f8d9f0343b93abff1b5f27999d
umbrae <umbrae@reddit.com>
2014-09-30 20:47:24 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+reddit-inbox-hush
+=================
+
+A very tiny chrome extension for folks annoyed by the inbox count color
---
+++ contentscript.js
@@ -0,0 +1,61 @@
+var hush = (function() {
+  var mailEl;
+  var countEl;
+  var messageCount;
+  var knownCount;
+
+  function getKnownCount() {
+    var c = window.localStorage.getItem('known-message-count');
+    if (c) {
+      return parseInt(c, 10);
+    } else {
+      return 0;
+    }
+  }
+
+  function setKnownCount(c) {
+    window.localStorage.setItem('known-message-count', c);
+  }
+
+  function hushCount() {
+    document.querySelector('.havemail').className = 'nohavemail';
+    countEl.className += " hushed";
+  }
+
+  function init() {
+    document.documentElement.className += " husher-start";
+
+    document.addEventListener('DOMContentLoaded', function() {
+      knownCount = getKnownCount();
+      mailEl = document.querySelector('#mail');
+      countEl = document.querySelector('.message-count');
+
+      if (countEl) {
+        messageCount = parseInt(countEl.innerText, 10);
+
+        if (messageCount === knownCount) {
+          hushCount();
+        } else {
+          var husher = document.createElement('a');
+          husher.className = 'husher';
+          husher.innerHTML = "☀︎";
+
+          husher.addEventListener('click', function() {
+            setKnownCount(messageCount);
+            hushCount();
+          });
+
+          mailEl.parentNode.insertBefore(husher, mailEl);
+        }
+      }
+
+      document.documentElement.className = document.documentElement.className.replace(' husher-start', '');
+    })
+  }
+
+  return {
+    "init": init
+  }
+}());
+
+hush.init();
